4ad98aa700c02eee098519b7199718796fa469b5
Carola Zapp <carola.zapp@gmx.net>
2023-03-23 13:53:31 +0100

    birthday and id canceled

--- src/components/E2D2ContactListGuests.jsx
+++ src/components/E2D2ContactListGuests.jsx
@@ -36,13 +36,13 @@ const E2D2ContactListGuests = () => {
                   <th>Nachname</th>
                   <th>email</th>
                   <th>verschickt</th>
-                  <th>Id</th>
+                  {/* <th>Id</th> */}
                   <th>Strasse</th>
                   <th>Hausnummer</th>
                   <th>Postleitzahl</th>
                   <th>Stadt</th>
                   <th>Telefon</th>
-                  <th>Geburtstag</th>
+                  {/* <th>Geburtstag</th> */}
                 </tr>
               </thead>
               <tbody>
@@ -52,13 +52,13 @@ const E2D2ContactListGuests = () => {
                     <td>{userguest.lastName}</td>
                     <td>{userguest.email}</td>
                     <td>{userguest.send ? "ja" : "nein"}</td>
-                    <td>{userguest._id}</td>
+                    {/* <td>{userguest._id}</td> */}
                     <td>{userguest.street}</td>
                     <td>{userguest.houseNumber}</td>
                     <td>{userguest.zipCode}</td>
                     <td>{userguest.city}</td>
                     <td>{userguest.telephone}</td>
-                    <td>{userguest.birthday}</td>
+                    {/* <td>{userguest.birthday}</td> */}
                   </tr>
                 ))}
               </tbody>
