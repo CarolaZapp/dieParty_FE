84dc4d7dd39a5368e76d2d41458d006f9306878b
Carola Zapp <carola.zapp@gmx.net>
2023-03-27 18:29:45 +0200

    reduce vite config

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    // ???? wie Verbindung zum Frontend?
+    // proxy: {
+    //   "/": {
+    //     target: "http://217.160.69.175:4173",
+    //     changeOrigin: true,
+    //   },
+    // },
     proxy: {
       "/api": {
         target: "http://217.160.69.175:4005",
-
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
